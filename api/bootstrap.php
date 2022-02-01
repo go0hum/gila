c74063572a104978e4ef667d92ecb3fea570ea89
Humberto Santos <1201hs@gmail.com>
2022-02-01 01:14:07 -0600

    validate if file exist

--- api/bootstrap.php
+++ api/bootstrap.php
@@ -5,7 +5,9 @@ require dirname(__DIR__) . "/vendor/autoload.php";
 set_error_handler("ErrorHandler::handleError");
 set_exception_handler("ErrorHandler::handleException");
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-$dotenv->load();
+if (file_exists(dirname(__DIR__) . "/.env")) {
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+}
 
 header("Content-type: application/json; charset: utf-8");
